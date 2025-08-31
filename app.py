12f0fc216e6fa027db4df59ed59e9fca015f6fbf
dinkabhakth <dinkabhakth@gmail.com>
2025-08-31 14:15:24 -0500

    Build failed due to permission issues. retriggering

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route('/')
 def wish():
     nessage = "Happy birthday {name}!"
-    return message.format(name=os.getenv('NAME', 'Kishor'))
+    return message.format(name=os.getenv('NAME', 'Kishor Karuvath'))
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
