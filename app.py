61a30e67d7cf7741458191a847313af502586382
dinkabhakth <dinkabhakth@gmail.com>
2025-08-31 13:58:55 -0500

    Adding more permission since the build failed

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route('/')
 def wish():
     nessage = "Happy birthday {name}!"
-    return message.format(name=os.getenv('NAME', 'Guest'))
+    return message.format(name=os.getenv('NAME', 'Kishor'))
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
